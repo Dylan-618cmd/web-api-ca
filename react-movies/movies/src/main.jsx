f210da21bf3802440d4f6f43559d8d7f7cb28106
Dylan <20106847@mail.wit.ie>
2025-12-18 18:43:04 +0000

    User authentication added

--- react-movies/movies/src/main.jsx
+++ react-movies/movies/src/main.jsx
@@ -1,15 +1,26 @@
-import React from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Route, Navigate, Routes} from "react-router";
+import HomePage from "./pages/homePage";
+import MoviePage from "./pages/movieDetailsPage";
+import FavoriteMoviesPage from "./pages/favoriteMoviesPage";
+import MovieReviewPage from "./pages/movieReviewPage";
+import SiteHeader from './components/siteHeader'
 import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import MoviesContextProvider from "./contexts/moviesContext";
+import AddMovieReviewPage from './pages/addMovieReviewPage';
+import UpcomingMoviesPage from "./pages/upcomingMoviePage";
+import TopRatedMoviesPage from "./pages/topRatedMoviesPage";
+import NowPlayingPage from "./pages/nowPlayingPage";
+import PopularMoviesPage from "./pages/popularMoviesPage";
+import RecommendedPage from "./pages/reccomendedMoviesPage";
 import LoginPage from "./pages/loginPage";
 import SignUpPage from "./pages/signUpPage";
 import StartPage from "./pages/startPage";
 import ProfilePage from "./pages/profilePage";
 import AuthContextProvider from "./contexts/authContext";
 import ProtectedRoutes from "./protectedRoutes";
-import HomePage from "./pages/homePage";
+
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -24,6 +35,7 @@ const queryClient = new QueryClient({
 const App = () => {
   return (
     <QueryClientProvider client={queryClient}>
+      <MoviesContextProvider>
       <BrowserRouter>
         <AuthContextProvider>
           <div className="container">
@@ -34,14 +46,24 @@ const App = () => {
               <Route path="/signup" element={< SignUpPage />} />
               <Route path="/profile" element={< ProfilePage />} />
               <Route element={<ProtectedRoutes />}>
-                <Route path ="/home" element={<HomePage />} />
+                  <Route path="/movies/favorites" element={<FavoriteMoviesPage />} />
+                  <Route path="/movies/upcoming" element={<UpcomingMoviesPage /> } />
+                  <Route path="/reviews/:id" element={ <MovieReviewPage /> } />
+                  <Route path="/movies/:id" element={<MoviePage />} />
+                  <Route path="/reviews/form" element={ <AddMovieReviewPage /> } />
+                  <Route path="/movies/top-rated" element={<TopRatedMoviesPage />} />
+                  <Route path="/movies/now-playing" element={<NowPlayingPage />} />
+                  <Route path="/movies/popular" element={<PopularMoviesPage />} />
+                  <Route path="/movies/:id/recommendations" element={<RecommendedPage />} />
+                  <Route path="/discover" element={<HomePage />} />
+                  <Route path="*" element={ <Navigate to="/" /> } />
               </Route>
               <Route path="*" element={<Navigate to="/" />} />
             </Routes>
           </div>
           </AuthContextProvider>
       </BrowserRouter>
-      
+    </MoviesContextProvider>
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   );
